df702ddf9cbdcf5fdd431644185bcc3320583081
raito <raitokomniak@gmail.com>
2022-09-21 13:35:19 +0300

    Init spring boot

--- src/main/java/com/example/demo/MyRestController.java
+++ src/main/java/com/example/demo/MyRestController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+
 /*Pieni Spring Boot -sovellus, joka hyödyntää edellä toteutettua rakennetta. Tähän osioon kuuluu lähinnä RESTrajapinta, joka tarjoaa käyttäjälle HTTP:n kautta käyttöliittymätoiminnot, kuten kurssien hakemisen
 selaimeen ja uuden tiedon lisääminen. Tiedon lisäämisen voi toteuttaa toimimaan esim. Postmanilla JSONkutsuina tai tehdä jopa html-lomakkeilla kevyen käyttöliittymän (staattiset sivut Spring Bootissa). Ohjelmaa
 voi laajentaa halutessaan tukemaan myös monipuolisempia toimintoja kuten opiskelijan lisääminen tietylle
@@ -9,12 +10,11 @@ kuitenkin suurempi arvioinnissa. */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
-@SpringBootApplication
 public class MyRestController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentService.class, args);
-	}
+   
 
 }
